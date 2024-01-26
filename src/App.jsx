08e4a2f2f20ba7fa7e767977205ca24fe34c341e
kaifata31 <87989976+kaifata31@users.noreply.github.com>
2024-01-26 10:19:35 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Studentui from './components/component/studentui'
+// import Studentpage from './components/component/studentpage'
+
+// import Login from './components/component/Login'
+// import Pswdreset from './components/component/pswdreset'
+
+
+function App() {
+
+  return (
+
+    <Studentui/>
+
+    // <Studentpage />
+  
+    // <div>
+    // <BrowserRouter>
+    // <Routes>
+    // <Route path='/' element={<Login/>}/>
+    // <Route path='/login' element={<Login/>}/>
+    // <Route path='/pswdreset' element={<Pswdreset/>}/>
+    // </Routes>
+    // </BrowserRouter>
+    // </div>
+
+
+  )
+}
+
+export default App
